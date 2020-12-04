facb457a55b5fcbb576b9df092cf1a38db4a9b9c
Manuel Negrete <58833064+ManuelNegCe@users.noreply.github.com>
2020-12-03 22:09:40 -0600

    Aquí intenté agregar el código para contectar a Mongoose

--- clase-3/index.js
+++ clase-3/index.js
@@ -3,6 +3,14 @@ const express = require('express')
 const { connect } = require('mongoose')
 const Router = require('./routers/index.js')
 
+// Connect to MongoDB
+mongoose.connect('mongodb://manuel:147369258@ds121825.mlab.com:11025/yourmongodb', { useNewUrlParser: true });
+mongoose.connection.once('open', function () {
+  console.log('Conection has been made!');
+}).on('error', function (error) {
+  console.log('Error is: ', error);
+});
+
 const app = express()
 const PORT = process.env.PORT || 3006
 
@@ -12,7 +20,7 @@ connect(process.env.MONGO_URI, options)
   .catch(err => console.log(`Error: ${err.message}`))
 
 app.use(express.json({ extended: true }))
-app.use('/api/v1' , Router)
+app.use('/api/v1', Router)
 
 app.get('/', (req, res) => {
   res.send('Te damos la bienvenida a nuestra API')
